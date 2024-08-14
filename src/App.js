3033ab5e68586b8ec7a2fb69dc14cc26872f7793
Hashim-09 <hashimkhan1775@gmail.com>
2024-08-14 17:20:57 +0530

    bult

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = ()=>{
+    if (mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#393A3F";
+      document.body.style.color = "white";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "#393A3F";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+          <Navbar title="TextUtils" mode={mode} toggleMode = {toggleMode} />
+          <div className="container my-3">
+            <Routes>
+                <Route exact path="/" element={<TextForm heading="Enter your Text Here"/>} />
+                <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+      </Router>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export default function TextForm(props) {
+let myStyle;
+
+if (props.mode === "dark") {
+  myStyle = {
+    backgroundColor : "#55565B",
+    color : "white",
+
+  }
+}
+else{
+  myStyle = {
+    backgroundColor : "white",
+  }
+}
+
+  const setUpper = ()=>{
+    setText(text.toUpperCase())
+  }
+
+  const setLower = ()=>{
+    setText(text.toLowerCase())
+  }
+
+  function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+  );
+}
+
+  const titelize = ()=>{
+    setText(toTitleCase(text))
+  }
+
+  const handleOnChange = (event)=>{
+    setText(event.target.value);
+  }
+
+  const clearText = (event)=>{
+    setText("");
+  }
+
+  const [text, setText] = useState("");
+  return (
+    <>
+    <div className="container">
+      <h1> {props.heading} </h1>
+      <div className="mb-3">
+        <textarea className="form-control" onChange={handleOnChange} style={myStyle} placeholder={"Enter text Here"} value={text} autoFocus={true} rows="12"></textarea>
+      </div>
+      <button className={`btn ${props.mode === "light" ? "btn-dark": "btn-secondary"} mx-3`} onClick={titelize}> Titelize Text </button>
+      <button className={`btn ${props.mode === "light" ? "btn-dark": "btn-secondary"} mx-3`} onClick={setUpper}> Capitalize Text </button>
+      <button className={`btn ${props.mode === "light" ? "btn-dark": "btn-secondary"} mx-3`} onClick={setLower}> Lower Case </button>
+      <button className={`btn ${props.mode === "light" ? "btn-dark": "btn-secondary"} mx-3`} onClick={clearText}> Clear Text </button>
+    </div>
+    <div className="container my-3">
+      <h2>Your Text Summary</h2>
+      <p> {text.split(" ").filter(n=>n).length} words and {text.length} characters </p>
+      <p> {Math.round(0.008 * text.split(" ").length)} minutes read </p>
+    </div>
+  </>
+  )
+}
+
+TextForm.propTypes = {
+  heading: PropTypes.string.isRequired,
+}
